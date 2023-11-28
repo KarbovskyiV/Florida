495356b419d7b1145758ce215439b739785a3416
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-28 14:26:25 +0200

    add create route

--- resources/js/router.js
+++ resources/js/router.js
@@ -5,6 +5,10 @@ const routes = [
         path: '/', component: () => import('./components/Product/Index.vue'),
         name: 'product.index'
     },
+    {
+        path: '/create', component: () => import('./components/Product/Create.vue'),
+        name: 'product.create'
+    },
 ]
 
 const router = createRouter({
