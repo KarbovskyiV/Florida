38c1c5530182dbaf19626c260b7bb186af86795e
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-28 13:43:22 +0200

    add basic product list display

--- resources/js/components/Product/Index.vue
+++ resources/js/components/Product/Index.vue
@@ -1,12 +1,51 @@
+<template>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Price</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="product in products">
+            <th>{{ product.id }}</th>
+            <td>{{ product.name }}</td>
+            <td>{{ product.description }}</td>
+            <td>{{ product.price }}</td>
+        </tr>
+        </tbody>
+    </table>
+</template>
+
 <script>
 export default {
-    name: "Index"
+    name: "Index",
+
+    data() {
+        return {
+            products: null
+        }
+    },
+
+    mounted() {
+        this.getProducts()
+    },
+
+    methods: {
+        getProducts() {
+            axios.get('/api/products')
+                .then(res => {
+                    this.products = res.data.data
+                })
+                .catch(error => {
+                    console.error('Error while fetching products:', error);
+                });
+        }
+    }
 }
 </script>
-
-<template>
-
-</template>
 
 <style scoped>
 
