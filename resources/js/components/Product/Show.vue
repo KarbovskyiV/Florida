220ef13476f6632256871eac7e04dd4e352dab63
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-28 16:46:19 +0200

    create basic show component

---
+++ resources/js/components/Product/Show.vue
@@ -0,0 +1,46 @@
+<template>
+    <div v-if="product">
+        <div>
+            Name: {{ this.product.name }}
+        </div>
+        <div>
+            Age: {{ this.product.description }}
+        </div>
+        <div>
+            Job: {{ this.product.price }}
+        </div>
+        <router-link :to="{name: 'product.index'}" class="btn btn-outline-secondary me-3">
+            To all
+        </router-link>
+        <router-link :to="{name: 'product.edit', params: {id: this.product.id}}" class="btn btn-outline-primary">Edit</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Show",
+
+    data() {
+        return {
+            product: null
+        }
+    },
+
+    mounted() {
+        this.getProduct()
+    },
+
+    methods: {
+        getProduct() {
+            axios.get(`/api/products/${this.$route.params.id}`)
+                .then(res => {
+                    this.product = res.data.data
+                })
+        },
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
