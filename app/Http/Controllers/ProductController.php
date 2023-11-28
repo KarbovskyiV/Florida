4f9c81431af9f65ed022eb17e4433beaf4a42d45
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-28 16:43:58 +0200

    realise show and update methods

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ProductRequest;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(): AnonymousResourceCollection
     {
         $products = Product::all();
 
@@ -23,14 +24,17 @@ class ProductController extends Controller
         return response([]);
     }
 
-    public function show()
+    public function show(Product $product): ProductResource
     {
-        //
+        return new ProductResource($product);
     }
 
-    public function update()
+    public function update(ProductRequest $request, Product $product)
     {
-        //
+        $data = $request->validated();
+        $product->update($data);
+
+        return response([]);
     }
 
     public function destroy()
