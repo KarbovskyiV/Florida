88196d9e7523c533d346ac780dfc591c6b98fb97
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-28 21:54:32 +0200

    add form, adjust store product method for image

--- resources/js/components/Product/Create.vue
+++ resources/js/components/Product/Create.vue
@@ -1,20 +1,26 @@
 <template>
     <div class="w-25">
-        <div class="mb-3">
-            <input type="text" v-model="name" placeholder="name" class="form-control">
-        </div>
-        <div class="mb-3">
-            <input type="text" v-model="description" placeholder="description" class="form-control">
-        </div>
-        <div class="mb-3">
-            <input type="number" v-model="price" placeholder="price" class="form-control">
-        </div>
-        <div class="mb-3">
-            <router-link :to="{name: 'product.index'}" class="btn btn-outline-secondary me-3">
-                Cancel
-            </router-link>
-            <input @click.prevent="storeProduct" type="submit" value="Create" class="btn btn-outline-primary">
-        </div>
+        <form @submit.prevent="storeProduct" enctype="multipart/form-data">
+            <div class="mb-3">
+                <input type="text" v-model="name" placeholder="name" class="form-control">
+            </div>
+            <div class="mb-3">
+                <input type="text" v-model="description" placeholder="description" class="form-control">
+            </div>
+            <div class="mb-3">
+                <input type="number" v-model="price" placeholder="price" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="image" class="form-label">Select Image</label>
+                <input type="file" id="image" ref="imageInput" @change="handleImageChange" class="form-control">
+            </div>
+            <div class="mb-3">
+                <router-link :to="{name: 'product.index'}" class="btn btn-outline-secondary me-3">
+                    Back
+                </router-link>
+                <button type="submit" class="btn btn-outline-primary">Create</button>
+            </div>
+        </form>
     </div>
 </template>
 
@@ -27,18 +33,33 @@ export default {
             name: '',
             description: '',
             price: null,
+            image: null,
         }
     },
 
     methods: {
         storeProduct() {
-            axios.post('/api/products', {name: this.name, description: this.description, price: this.price,})
+            const formData = new FormData();
+            formData.append('name', this.name);
+            formData.append('image', this.image);
+            formData.append('description', this.description);
+            formData.append('price', this.price);
+
+            axios.post('/api/products', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            })
                 .then(res => {
                     this.$router.push({name: 'product.index'})
                 })
                 .catch(error => {
                     console.error('Error while creating product:', error);
                 });
+        },
+
+        handleImageChange(event) {
+            this.image = event.target.files[0];
         }
     }
 }
