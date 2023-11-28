82ea5d9cfe6f1e66c3d127f250ee001228998617
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-28 16:43:24 +0200

    add edit and show route

--- resources/js/router.js
+++ resources/js/router.js
@@ -9,6 +9,14 @@ const routes = [
         path: '/create', component: () => import('./components/Product/Create.vue'),
         name: 'product.create'
     },
+    {
+        path: '/:id/edit', component: () => import('./components/Product/Edit.vue'),
+        name: 'product.edit'
+    },
+    {
+        path: '/:id', component: () => import('./components/Product/Show.vue'),
+        name: 'product.show'
+    },
 ]
 
 const router = createRouter({
